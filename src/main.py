97278dfc0d5705a4db262f87e44c2c1e6bd2a7c9
luisgaboardi <luis.ggaboardi@gmail.com>
2023-01-27 19:13:31 -0300

    Step 2: Docker Compose with containers connected

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-import json
 import os
+import json
 
 from parser.YAML_parser import YAMLParser
 from parser.feature_engineering_parser import FeatureEngineeringParser
 from parser.model_parser import ModelParser
+
 
 if __name__ != "__main__":
     exit()   
@@ -16,21 +17,19 @@ def get_config():
     for file in os.listdir('src/yamls'):
         filepath = os.path.join('src/yamls', file)
         config = initialParser(filepath).parse()
-    
+
         features_configs, columns_set_alias = featureEngineringParser(filepath).parse(config['feature_engineering'])
         del config['feature_engineering']
-        json_features_config = json.dumps(json.loads(features_configs), indent=2)
-        
+
         model_configs = modelParser(columns_set_alias).parse(config['model'])
         del config['model']
-        json_model_configs = json.dumps(json.loads(model_configs), indent=2)
 
-        
-        print("FEATURES")
-        print(json_features_config)
-        print(2*'\n')
+        print("FEATURES\n")
+        print(features_configs)
+        print(3*'\n')
         print(20*'-')
-        print(2*'\n')
-        print(json_model_configs)
-         
+        print(3*'\n')
+        print("MODELS\n")
+        print(model_configs)
+
 get_config()
